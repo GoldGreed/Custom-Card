ce1402ac1ff4b370994f1f644ee22bca0f1c2f39
GoldGreed <omicciolisilvio@gmail.com>
2024-10-10 16:54:13 +0200

    c

--- script/c29713285.lua
+++ script/c29713285.lua
@@ -1,13 +1,12 @@
---Blue-Eyes Xyz Dimension Dragon
+-- Blue-Eyes Xyz Dimension Dragon
 local s,id=GetID()
 function s.initial_effect(c)
-	--Xyz Summon procedure
-	Xyz.AddProcedure(c,nil,8,2,nil,nil,99)
+	-- Xyz Summon procedure (2 Level 8 Dragon-Type monsters)
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_DRAGON),8,2)
 	c:EnableReviveLimit()
 
-	-- Destroy 1 card on Xyz Summon
+	-- Destroy 1 card on the field when Xyz Summoned
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_DESTROY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -16,21 +15,19 @@ function s.initial_effect(c)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
 
-	-- Detach 1 material to Special Summon 1 Level 1 Tuner and 1 "Blue-Eyes"
+	-- Detach 1 material: Special Summon 1 Level 1 Tuner and 1 "Blue-Eyes" monster
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetCost(s.sscost)
-	e2:SetTarget(s.sstg)
-	e2:SetOperation(s.ssop)
+	e2:SetCost(s.spcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
 
-	-- Negate destruction effect (Quick Effect)
+	-- Negate a card or effect that would destroy a monster
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
 	e3:SetCategory(CATEGORY_NEGATE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_CHAINING)
@@ -42,125 +39,217 @@ function s.initial_effect(c)
 	e3:SetOperation(s.negop)
 	c:RegisterEffect(e3)
 
-	-- Add 1 "Blue-Eyes" card and Special Summon 1 "Blue-Eyes" when sent to GY
+	-- Special Summon 2 "Blue-Eyes" monsters during Main Phase
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,3))
-	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_TO_GRAVE)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1,{id,3})
-	e4:SetTarget(s.thtg)
-	e4:SetOperation(s.thop)
+	e4:SetTarget(s.spbtg)
+	e4:SetOperation(s.spbop)
 	c:RegisterEffect(e4)
 
-	-- Banish to prevent "Blue-Eyes" destruction by battle or card effect
+	-- Target 1 opponent's monster; its ATK becomes 0
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,4))
-	e5:SetCategory(CATEGORY_PROTECT)
-	e5:SetType(EFFECT_TYPE_QUICK_O)
-	e5:SetRange(LOCATION_GRAVE)
-	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetCategory(CATEGORY_ATKCHANGE)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_MZONE)
 	e5:SetCountLimit(1,{id,4})
-	e5:SetCondition(s.procon)
-	e5:SetOperation(s.proop)
+	e5:SetTarget(s.atktg)
+	e5:SetOperation(s.atkop)
 	c:RegisterEffect(e5)
+
+	-- Add card that mentions "Blue-Eyes White Dragon" when sent to GY
+	local e6=Effect.CreateEffect(c)
+	e6:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetCode(EVENT_TO_GRAVE)
+	e6:SetCountLimit(1,{id,5})
+	e6:SetTarget(s.thtg)
+	e6:SetOperation(s.thop)
+	c:RegisterEffect(e6)
+
+	-- Special Summon this card from GY
+	local e7=Effect.CreateEffect(c)
+	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e7:SetType(EFFECT_TYPE_IGNITION)
+	e7:SetRange(LOCATION_GRAVE)
+	e7:SetCountLimit(1,{id,6})
+	e7:SetCondition(s.spselfcon)
+	e7:SetTarget(s.spselftg)
+	e7:SetOperation(s.spselfop)
+	c:RegisterEffect(e7)
+
+	-- Protect "Blue-Eyes" monsters by banishing this card from GY
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e8:SetRange(LOCATION_GRAVE)
+	e8:SetCode(EFFECT_DESTROY_REPLACE)
+	e8:SetTarget(s.reptg)
+	e8:SetOperation(s.repop)
+	c:RegisterEffect(e8)
+
+	-- If this card is banished, return it to the GY
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e9:SetCode(EVENT_REMOVE)
+	e9:SetOperation(s.retgyop)
+	c:RegisterEffect(e9)
 end
 
--- Effect 1: Destroy 1 card on Xyz Summon
+-- Destroy 1 card on the field upon Xyz Summon
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
 end
+
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,0,0)
 end
+
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
 	end
 end
 
--- Effect 2: Detach 1 material to Special Summon 1 Level 1 Tuner and 1 "Blue-Eyes"
-function s.sscost(e,tp,eg,ep,ev,re,r,rp,chk)
+-- Detach 1 material: Special Summon 1 Level 1 Tuner and 1 "Blue-Eyes" monster
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function s.tunerfilter(c,e,tp)
-	return c:IsType(TYPE_TUNER) and c:IsLevel(1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+
+function s.spfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.befilter(c,e,tp)
-	return c:IsSetCard(0xdd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-		and Duel.IsExistingMatchingCard(s.tunerfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
-		and Duel.IsExistingMatchingCard(s.befilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK+LOCATION_GRAVE)
 end
-function s.ssop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
-	local g1=Duel.SelectMatchingCard(tp,s.tunerfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
-	local g2=Duel.SelectMatchingCard(tp,s.befilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	local g1=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+	local g2=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if #g1>0 and #g2>0 then
 		Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP)
 		Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 
--- Effect 3: Negate destruction effect
+-- Negate destruction effect
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:IsHasCategory(CATEGORY_DESTROY) and Duel.IsChainNegatable(ev)
+	local rc=re:GetHandler()
+	return re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_MONSTER),tp,LOCATION_MZONE,0,1,nil)
 end
+
 function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
+
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 end
+
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) then
-		-- Optional: Destroy the card if you wish to add destruction.
-		-- Duel.Destroy(eg,REASON_EFFECT)
+		Duel.Destroy(eg,REASON_EFFECT)
 	end
 end
 
--- Effect 4: Add 1 "Blue-Eyes" card and Special Summon 1 "Blue-Eyes" when sent to GY
+-- Special Summon 2 "Blue-Eyes" monsters during Main Phase
+function s.spbfilter(c,e,tp)
+	return c:IsCode(0xdd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.spbtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsExistingMatchingCard(s.spbfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,2,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+
+function s.spbop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,s.spbfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,2,2,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+-- Target 1 face-up monster and reduce its ATK to 0
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsControler,1-tp),tp,0,LOCATION_MZONE,1,nil) end
+end
+
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.SelectMatchingCard(tp,aux.FaceupFilter(Card.IsControler,1-tp),tp,0,LOCATION_MZONE,1,1,nil):GetFirst()
+	if tc and tc:IsFaceup() then
+		local atk=tc:GetBaseAttack()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
+
+-- Add 1 "Blue-Eyes White Dragon" card and Special Summon 1 "Blue-Eyes"
 function s.thfilter(c)
-	return c:IsAbleToHand() and c:IsType(TYPE_MONSTER) and c:IsSetCard(0xdd)
+	return c:IsCode(0xdd) and c:IsAbleToHand()
 end
-function s.spfilter(c,e,tp)
-	return c:IsSetCard(0xdd) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
+
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
+
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g1=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g1>0 then
-		Duel.SendtoHand(g1,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g1)
-		Duel.BreakEffect()
-		local g2=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-		if #g2>0 then
-			Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
-		end
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
 
--- Effect 5: Banish to prevent destruction of "Blue-Eyes" by battle or card effect
-function s.procon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0xdd),tp,LOCATION_MZONE,0,1,nil)
+-- Special Summon this card from the GY
+function s.spselfcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnCount()>e:GetHandler():GetTurnID()
 end
-function s.proop(e,tp,eg,ep,ev,re,r,rp)
+
+function s.spselftg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+
+function s.spselfop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+-- Protect "Blue-Eyes" by banishing this card instead of destruction
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=eg:Filter(aux.FilterBoolFunction(Card.IsSetCard,0xdd),nil)
+	if chk==0 then return g:IsExists(aux.TRUE,1,nil) and e:GetHandler():IsAbleToRemove() end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
+
+-- Return this card to GY if banished
+function s.retgyop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SendtoGrave(e:GetHandler(),REASON_EFFECT+REASON_RETURN)
 end
