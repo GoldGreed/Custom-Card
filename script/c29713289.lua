b6f066994b36b5dd427bd61e8e8cda88be0089da
GoldGreed <omicciolisilvio@gmail.com>
2024-10-17 10:42:33 +0200

    Update c29713289.lua

--- script/c29713289.lua
+++ script/c29713289.lua
@@ -138,7 +138,7 @@ end
 
 -- Effect 2: Special Summon from GY if a Dragon is on the field
 function s.spgycon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsRace,RACE_DRAGON),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsRace,RACE_DRAGON),tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
 end
 function s.spgytg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
