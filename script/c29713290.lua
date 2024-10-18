8281d3ea3bd06ad465b87928a2fc9b90a40ddc6d
GoldGreed <omicciolisilvio@gmail.com>
2024-10-18 16:42:28 +0200

    c

--- script/c29713290.lua
+++ script/c29713290.lua
@@ -1,5 +1,277 @@
---Galaxy-Eyes Tachyon Revolution Dragon 
-local s,id,o=GetID()
+-- Galaxy-Eyes Tachyon Revolution Dragon
+local s,id=GetID()
 function s.initial_effect(c)
-	
+	-- Special Summon from hand in Defense Position, ATK/DEF become 0
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon1)
+	e1:SetTarget(s.sptg1)
+	e1:SetOperation(s.spop1)
+	c:RegisterEffect(e1)
+
+	-- Special Summon from GY if Dragon Xyz Monster is on the field or GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.spcon2)
+	e2:SetTarget(s.sptg2)
+	e2:SetOperation(s.spop2)
+	c:RegisterEffect(e2)
+
+	-- Send 1 "Galaxy-Eyes" to GY, add 1 "Galaxy-Eyes" and 1 "Tachyon" to hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_TOGRAVE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e4)
+
+	-- Increase the Level of all monsters you control
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,3))
+	e5:SetCategory(CATEGORY_TOGRAVE+CATEGORY_LVCHANGE)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,id)
+	e5:SetTarget(s.lvtg)
+	e5:SetOperation(s.lvop)
+	c:RegisterEffect(e5)
+
+	-- Special Summon 1 Dragon from GY and 1 from Deck
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,4))
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCountLimit(1,id)
+	e6:SetTarget(s.sptg3)
+	e6:SetOperation(s.spop3)
+	c:RegisterEffect(e6)
+
+	-- Add 1 "Galaxy" or "Tachyon" card on Xyz Summon, Special Summon 2 Dragons
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,5))
+	e7:SetCategory(CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+	e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e7:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e7:SetProperty(EFFECT_FLAG_DELAY)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetCountLimit(1,id)
+	e7:SetCondition(s.xyzcon)
+	e7:SetTarget(s.xyztg)
+	e7:SetOperation(s.xyzop)
+	c:RegisterEffect(e7)
+
+	-- Quick Effect: Banish this card to protect all monsters from destruction
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,6))
+	e8:SetType(EFFECT_TYPE_QUICK_O)
+	e8:SetRange(LOCATION_GRAVE)
+	e8:SetCode(EVENT_FREE_CHAIN)
+	e8:SetCountLimit(1,id)
+	e8:SetCost(aux.bfgcost) -- Banish itself
+	e8:SetOperation(s.protectop)
+	c:RegisterEffect(e8)
+
+	-- Xyz Monster using this card gains indestructibility
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_SINGLE)
+	e9:SetCode(EFFECT_XYZ_MATERIAL)
+	e9:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e9:SetValue(s.matfilter)
+	c:RegisterEffect(e9)
 end
+
+-- Special Summon from hand in Defense Position, ATK/DEF become 0
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		-- Set ATK/DEF to 0
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetValue(0)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_DEFENSE)
+		c:RegisterEffect(e2)
+	end
+end
+
+-- Special Summon from GY if Dragon Xyz Monster is on the field or GY
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,1,nil)
+end
+function s.xyzfilter(c)
+	return c:IsType(TYPE_XYZ) and c:IsRace(RACE_DRAGON)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
+
+-- Search for "Galaxy-Eyes" and "Tachyon" cards
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.galaxyfilter,tp,LOCATION_DECK,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.tachyonfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g1=Duel.SelectMatchingCard(tp,s.galaxyfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g2=Duel.SelectMatchingCard(tp,s.tachyonfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g1>0 and #g2>0 then
+		Duel.SendtoHand(g1,nil,REASON_EFFECT)
+		Duel.SendtoHand(g2,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g1+g2)
+	end
+end
+function s.galaxyfilter(c)
+	return c:IsSetCard(0x107b) and c:IsAbleToHand()
+end
+function s.tachyonfilter(c)
+	return c:IsSetCard(SET_TACHYON) and c:IsAbleToHand()
+end
+
+-- Increase Level of all monsters you control by the number of Dragons sent to GY
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.dragonfilter,tp,LOCATION_DECK,0,2,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,2,tp,LOCATION_DECK)
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.dragonfilter,tp,LOCATION_DECK,0,2,2,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+		local lvl=2 -- Adjust based on the number of cards sent to GY
+		local c=e:GetHandler()
+		local tg=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+		for tc in aux.Next(tg) do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_LEVEL)
+			e1:SetValue(lvl)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+		end
+	end
+end
+function s.dragonfilter(c)
+	return c:IsRace(RACE_DRAGON) and c:IsAbleToGrave()
+end
+
+-- Special Summon 1 Dragon from GY and 1 from Deck
+function s.sptg3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_GRAVE+LOCATION_DECK)
+end
+function s.spop3(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g1=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g2=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_DECK,0,1,1,nil)
+	if #g1>0 and #g2>0 then
+		local tc1=g1:GetFirst()
+		local tc2=g2:GetFirst()
+		if Duel.SpecialSummonStep(tc1,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+			-- Halve ATK/DEF of the first monster
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetValue(tc1:GetAttack()/2)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc1:RegisterEffect(e1)
+			local e2=e1:Clone()
+			e2:SetCode(EFFECT_SET_DEFENSE_FINAL)
+			tc1:RegisterEffect(e2)
+		end
+		if Duel.SpecialSummonStep(tc2,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+			-- Halve ATK/DEF of the second monster
+			local e3=Effect.CreateEffect(e:GetHandler())
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e3:SetValue(tc2:GetAttack()/2)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc2:RegisterEffect(e3)
+			local e4=e3:Clone()
+			e4:SetCode(EFFECT_SET_DEFENSE_FINAL)
+			tc2:RegisterEffect(e4)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+
+-- Add 1 "Galaxy" or "Tachyon" card on Xyz Summon, Special Summon 2 Dragons
+function s.xyzcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(Card.IsType,1,nil,TYPE_XYZ)
+end
+function s.xyztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.galaxyfilter,tp,LOCATION_DECK,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.tachyonfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_GRAVE+LOCATION_DECK)
+end
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g1=Duel.SelectMatchingCard(tp,s.galaxyfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g2=Duel.SelectMatchingCard(tp,s.tachyonfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g1>0 and #g2>0 then
+		Duel.SendtoHand(g1,nil,REASON_EFFECT)
+		Duel.SendtoHand(g2,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g1+g2)
+	end
+
+	-- Special Summon 2 Dragons
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg1=Duel.SelectMatchingCard(tp,s.dragonfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local sg2=Duel.SelectMatchingCard(tp,s.dragonfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #sg1>0 and #sg2>0 then
+		Duel.SpecialSummon(sg1,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		Duel.SpecialSummon(sg2,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+	end
+end
+
+-- Quick Effect: Banish this card to protect all monsters from destruction
+function s.protectop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE,0,nil)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+
+-- Xyz Monster using this card gains indestructibility
+function s.matfilter(e,c)
+	return c:IsRace(RACE_DRAGON) and c:IsType(TYPE_XYZ)
+end
+
